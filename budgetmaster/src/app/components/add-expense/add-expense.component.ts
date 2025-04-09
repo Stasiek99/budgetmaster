cb64066f136e36bd6603d722662bc651f3a67edc
Stasiek99 <stasiekpilich@gmail.com>
2025-04-09 14:47:35 +0200

    feature: implemented add-expense.component

--- budgetmaster/src/app/components/add-expense/add-expense.component.ts
+++ budgetmaster/src/app/components/add-expense/add-expense.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
+import { MatCard } from "@angular/material/card";
+import { MatFormField } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { MatButton } from "@angular/material/button";
+
 @Component({
   selector: 'app-add-expense',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatFormField,
+    MatInput,
+    FormsModule,
+    MatButton
+  ],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.scss'
 })
 export class AddExpenseComponent {
+  name = "";
+  amount: number | null = null;
 
+  add(): void {
+    console.log("Add: ", this.name, this.amount);
+  }
 }
